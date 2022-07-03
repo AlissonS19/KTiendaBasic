9e38c68eb5a8357410e4da3b7d0ed72891408df7
unknown <AlissonS19>
2022-07-03 18:35:47 -0500

    primer commit

---
+++ accion.js
@@ -0,0 +1,28 @@
+let dia = 2;
+switch (dia) {
+    case 0:
+	    console.log("Lunes")
+	break;
+	case 1:
+	    console.log("Martes")
+    break;
+	case 2:
+		console.log("Miercoles")
+    break;
+	case 3:
+		console.log("Jueves")
+    break;
+	case 4:
+		console.log("Viernes")
+    break;
+	case 5:
+		console.log("Sabado")
+    break;
+	case 6:
+		console.log("Domingo")
+    break;
+
+	default:
+        console.log("El día no existe")
+	break;
+	}
